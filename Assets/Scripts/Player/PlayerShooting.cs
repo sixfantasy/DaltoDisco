fed48bad4865475e351337e541cc5e6b6d9fe128
Alex Nadal <alex.nadal.7e3@itb.cat>
2023-01-12 17:46:09 +0100

    Shooting cooldown i nota nova

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -7,6 +7,8 @@ public class PlayerShooting : MonoBehaviour
     public GameObject projectile;
     public float damage;
     public float projectileForce;
+    public float shootCooldown = 3;
+    private bool readyToFire = true;
     private BoxCollider2D collider2D;
     Vector2 shootCorrection = new Vector2(0, -2);
 
@@ -17,7 +19,12 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && collider2D != null)
+        Shoot();
+       
+    }
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0) && collider2D != null && readyToFire==true)
         {
             GameObject spawn = Instantiate(projectile, collider2D.bounds.center, Quaternion.identity);
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -25,6 +32,13 @@ public class PlayerShooting : MonoBehaviour
             Vector2 direction = (mousePos - myPos + shootCorrection).normalized;
             spawn.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
             spawn.GetComponent<projectiles>().damage = damage;
+            readyToFire = false;
+            StartCoroutine(FireCooldown());
         }
+    }
+    IEnumerator FireCooldown()
+    {
+        yield return new WaitForSeconds(shootCooldown);
+        readyToFire = true;
     }
 }
