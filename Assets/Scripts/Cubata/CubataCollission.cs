32a8de584ec90206f115aec25b8c489433f2f6a5
Alex Nadal <alex.nadal.7e3@itb.cat>
2023-01-17 15:38:29 +0100

    CubataEffect

---
+++ Assets/Scripts/Cubata/CubataCollission.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubataCollission : MonoBehaviour
+{
+    public float cubataHeal;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            CubataManager.Instance.isDrunk=true;
+            PlayerStats.playerStats.HealCharacter(cubataHeal);
+            Destroy(this.gameObject);
+        }
+    }
+}
